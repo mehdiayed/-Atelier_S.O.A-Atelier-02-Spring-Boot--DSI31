48db2a5865ba0d5d288171d85186c19aabb981c9
mehdiayed <81702423+mehdiayed@users.noreply.github.com>
2022-10-16 21:01:39 +0200

    Persister et interroger les données avec Spring Data JPA
    Objectifs :
    1. Créer une association OneToMany entre deux entités,
    2. Utilisation de Lombok,
    3. Interroger les entités en fournissant un attribut non clé,
    4. Ecrire des requêtes @Query en utilisant le langage JPQL,
    5. Ecrire des requêtes @Query en passant des entités en paramètre,
    6. Interroger les produits selon l’id de leur catégorie,
    7. Trier les données,
    8. Ajouter les méthodes du Repository à la couche Service.

---
+++ produitsV2/src/main/java/com/example/demo/ProduitsV2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProduitsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProduitsV2Application.class, args);
+	}
+
+}
